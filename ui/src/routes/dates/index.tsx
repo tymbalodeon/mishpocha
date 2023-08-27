216d8b594a11d1bd923fe8b67efc8fd5145f4974
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-08-26 22:52:18 -0400

    update ui

--- ui/src/routes/dates/index.tsx
+++ ui/src/routes/dates/index.tsx
@@ -1,20 +1,65 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+const dates = [
+    {
+        "local_date": "1939-11-03",
+        "birthdays": [
+            "Joe McPhee"
+        ]
+    },
+    {
+        "local_date": "1944-04-05",
+        "birthdays": [
+            "Evan Parker"
+        ]
+    },
+    {
+        "local_date": "1947-03-08",
+        "birthdays": [
+            "Paul Lytton"
+        ]
+    },
+    {
+        "local_date": "1947-04-22",
+        "birthdays": [
+            "Barry Guy"
+        ]
+    },
+    {
+        "local_date": "1995-06-18",
+        "birthdays": []
+    }
+];
+
 export default component$(() => {
-  return (
-    <>
-      <div>Test</div>
-    </>
-  );
+    return (
+        <>
+            <h2>Dates</h2>
+            {
+                dates ? (
+                    dates.map(date => {
+                        return (
+                            <div class="card w-96 bg-neutral shadow-xl m-4">
+                                <div class="card-body">
+                                    <h2 class="card-title">{date.local_date}</h2>
+                                    <p>Births: {date.birthdays}</p>
+                                </div>
+                            </div>
+                        )
+                    })
+                ) : null
+            }
+        </>
+    )
 });
 
 export const head: DocumentHead = {
-  title: "Mishpocha Database | Dates",
-  meta: [
-    {
-      name: "Mishpocha Database | Dates",
-      content: "Mishpocha Database | Dates",
-    },
-  ],
+    title: "Mishpocha Database | Dates",
+    meta: [
+        {
+            name: "Mishpocha Database | Dates",
+            content: "Mishpocha Database | Dates",
+        },
+    ],
 };
--- ui/src/routes/index.tsx
+++ ui/src/routes/index.tsx
@@ -1,85 +1,39 @@
 import { component$ } from "@builder.io/qwik";
 import {
-  useContent,
-  useLocation,
-  type DocumentHead,
-  routeLoader$,
+    useContent,
+    useLocation,
+    type DocumentHead,
+    routeLoader$,
 } from "@builder.io/qwik-city";
 
 export const useGetApiData = routeLoader$(async (requestEvent) => {
-  const apiDomain = requestEvent.env.get("API_DOMAIN");
+    const apiDomain = requestEvent.env.get("API_DOMAIN");
 
-  try {
-    const response = await fetch(`http://${apiDomain}`);
-    const data = await response.json();
-    return data.message;
-  } catch (error) {
-    return "ERROR: Failed to load data.";
-  }
+    try {
+        const response = await fetch(`http://${apiDomain}`);
+        const data = await response.json();
+        return data.message;
+    } catch (error) {
+        return "ERROR: Failed to load data.";
+    }
 });
 
 export default component$(() => {
-  const { menu } = useContent();
-  const pathname = useLocation().url.pathname;
-  const apiMessage = useGetApiData();
+    const apiMessage = useGetApiData();
 
-  return (
-    <div class="drawer lg:drawer-open">
-      <input id="drawer" type="checkbox" class="drawer-toggle" />
-      <div class="drawer-content flex flex-col items-center justify-center">
-        <div>
-          This message has been loaded from the server: {apiMessage.value}
-        </div>
-        <label for="drawer" class="btn btn-primary drawer-button lg:hidden">
-          Open drawer
-        </label>
-      </div>
-      <div class="drawer-side">
-        <label for="drawer" class="drawer-overlay"></label>
-        <ul class="menu p-4 w-80 h-full bg-base-200 text-base-content">
-          <h1>{menu.text}</h1>
-          {menu
-            ? menu.items?.map((item, index) => {
-                const title = item.text;
-                const items = item.items;
-
-                return (
-                  <>
-                    <p class="menu-label">{title}</p>
-                    <ul class="menu bg-base-200 w-56 rounded-box">
-                      {items?.map((item) => {
-                        const href = item.href;
-
-                        return (
-                          <li key={index}>
-                            <a
-                              href={item.href}
-                              class={{
-                                "is-active": pathname === href,
-                              }}
-                            >
-                              {item.text}
-                            </a>
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </>
-                );
-              })
-            : null}
-        </ul>
-      </div>
-    </div>
-  );
+    return (
+        <>
+            This message has been loaded from the server: {apiMessage.value}
+        </>
+    );
 });
 
 export const head: DocumentHead = {
-  title: "Mishpocha Database",
-  meta: [
-    {
-      name: "Mishpocha Database",
-      content: "Mishpocha Database",
-    },
-  ],
+    title: "Mishpocha Database",
+    meta: [
+        {
+            name: "Mishpocha Database",
+            content: "Mishpocha Database",
+        },
+    ],
 };
