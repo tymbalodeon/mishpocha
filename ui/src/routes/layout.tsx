38c0468491c8ce9846e21367267676a74f616924
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-09-07 22:22:18 -0400

    update mobile menu

--- ui/src/routes/layout.tsx
+++ ui/src/routes/layout.tsx
@@ -28,10 +28,33 @@ export default component$(() => {
       <div class="drawer lg:drawer-open">
         <input id="drawer" type="checkbox" class="drawer-toggle" />
         <div class="drawer-content">
+          <div class="drawer-button lg:hidden">
+            <div class="navbar bg-base-200">
+              <div class="flex-1">
+                <a class="btn btn-ghost normal-case text-xl">{menu?.text}</a>
+              </div>
+              <div class="flex-none">
+                <label for="drawer">
+                  <button class="btn btn-square btn-ghost">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      class="inline-block w-5 h-5 stroke-current"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      ></path>
+                    </svg>
+                  </button>
+                </label>
+              </div>
+            </div>
+          </div>
           <Slot />
-          <label for="drawer" class="btn btn-primary drawer-button lg:hidden">
-            Open drawer
-          </label>
         </div>
         <div class="drawer-side">
           <label for="drawer" class="drawer-overlay"></label>
