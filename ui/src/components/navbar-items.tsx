345886b2fadfe8933f8b0312b0e20dce8eec0311
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-09-19 18:16:26 -0400

    fix menu

--- ui/src/components/navbar-items.tsx
+++ ui/src/components/navbar-items.tsx
@@ -21,67 +21,50 @@ export const NavBarItems = component$<NavBarItemsProps>((props) => {
     const items = props.items;
 
     return (
-        <div class="w-vw">
-            <ul
-                id={!props.navbar ? "" : "navbar-items"}
-                class={`menu ${
-                    !props.navbar
-                        ? "p-8 w-62 h-full"
-                        : "menu-horizontal bg-base-200 hidden lg:hidden"
-                }`}
-            >
-                {items
-                    ? items.map((item) => {
-                          const title = item.text || "";
-                          const items = item.items || "";
+        <ul
+            id={props.navbar ? "navbar-items" : ""}
+            class={`menu ${
+                props.navbar
+                    ? "menu-horizontal bg-base-200 hidden lg:hidden"
+                    : ""
+            }`}
+        >
+            {items
+                ? items.map((item) => {
+                      const title = item.text || "";
+                      const items = item.items || "";
 
-                          return (
-                              <li key={title}>
-                                  <div>
-                                      <details open>
-                                          <summary>{title}</summary>
-                                          <ul>
-                                              {items
-                                                  ? items?.map(
-                                                          (
-                                                              item: ContentMenu,
-                                                          ) => {
-                                                              const href =
-                                                                  item.href;
+                      return (
+                          <li key={title}>
+                              <details open>
+                                  <summary>{title}</summary>
+                                  <ul>
+                                      {items
+                                          ? items?.map((item: ContentMenu) => {
+                                                  const href = item.href;
 
-                                                              return (
-                                                                  <li
-                                                                      key={
-                                                                          href
-                                                                      }
-                                                                  >
-                                                                      <a
-                                                                          href={
-                                                                              href
-                                                                          }
-                                                                          class={{
-                                                                              "is-active":
-                                                                                  pathname ===
-                                                                                  href,
-                                                                          }}
-                                                                      >
-                                                                          {
-                                                                              item.text
-                                                                          }
-                                                                      </a>
-                                                                  </li>
-                                                              );
-                                                          },
-                                                      )
-                                                  : null}
-                                          </ul>
-                                      </details>
-                                  </div>
-                              </li>
-                          );
-                      })
-                    : null}
-            </ul>
-        </div>
+                                                  return (
+                                                      <li key={href}>
+                                                          <a
+                                                              href={href}
+                                                              class={{
+                                                                  active:
+                                                                      pathname ===
+                                                                      href,
+                                                              }}
+                                                          >
+                                                              {item.text}
+                                                          </a>
+                                                      </li>
+                                                  );
+                                              })
+                                          : null}
+                                  </ul>
+                              </details>
+                          </li>
+                      );
+                  })
+                : null}
+        </ul>
     );
 });
