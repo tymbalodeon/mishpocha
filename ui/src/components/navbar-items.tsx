bdec9ac8494bc47fde35d10ae587cd06b5eaf6e5
Ben Rosen <benjamin.j.rosen@gmail.com>
2023-09-19 20:28:56 -0400

    fix menu

--- ui/src/components/navbar-items.tsx
+++ ui/src/components/navbar-items.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, $, type QwikMouseEvent } from "@builder.io/qwik";
 import { useLocation, type ContentMenu } from "@builder.io/qwik-city";
 
 interface MenuLink {
@@ -16,10 +16,31 @@ interface NavBarItemsProps {
     navbar?: boolean;
 }
 
+const toggleDropdown = (event: QwikMouseEvent) => {
+    const isNavbar = Boolean(
+        (event.target as HTMLElement).id.includes("navbar"),
+    );
+    let Ids = ["menu-dropdown", "menu-dropdown-toggle"];
+
+    if (isNavbar) {
+        Ids = Ids.map((id: string) => `navbar-${id}`);
+    }
+
+    const dropdowns = Ids.map((id: string) => document.getElementById(id));
+
+    dropdowns.forEach((dropdown: HTMLElement | null) => {
+        const showClass = "menu-dropdown-show";
+        if (dropdown?.classList.contains(showClass)) {
+            dropdown.classList.remove(showClass);
+        } else {
+            dropdown?.classList.add(showClass);
+        }
+    });
+};
+
 export const NavBarItems = component$<NavBarItemsProps>((props) => {
     const pathname = useLocation().url.pathname;
     const items = props.items;
-
     return (
         <ul
             id={props.navbar ? "navbar-items" : ""}
@@ -35,32 +56,42 @@ export const NavBarItems = component$<NavBarItemsProps>((props) => {
                       const items = item.items || "";
 
                       return (
-                          <li key={title}>
-                              <details open>
-                                  <summary>{title}</summary>
-                                  <ul>
-                                      {items
-                                          ? items?.map((item: ContentMenu) => {
-                                                  const href = item.href;
+                          <li key={title} onClick$={$(toggleDropdown)}>
+                              <span
+                                  id={`${
+                                      props.navbar ? "navbar-" : ""
+                                  }menu-dropdown-toggle`}
+                                  class="menu-dropdown-toggle menu-dropdown-show"
+                              >
+                                  {title}
+                              </span>
+                              <ul
+                                  id={`${
+                                      props.navbar ? "navbar-" : ""
+                                  }menu-dropdown`}
+                                  class="menu-dropdown menu-dropdown-show"
+                              >
+                                  {items
+                                      ? items?.map((item: ContentMenu) => {
+                                              const href = item.href;
 
-                                                  return (
-                                                      <li key={href}>
-                                                          <a
-                                                              href={href}
-                                                              class={{
-                                                                  active:
-                                                                      pathname ===
-                                                                      href,
-                                                              }}
-                                                          >
-                                                              {item.text}
-                                                          </a>
-                                                      </li>
-                                                  );
-                                              })
-                                          : null}
-                                  </ul>
-                              </details>
+                                              return (
+                                                  <li key={href}>
+                                                      <a
+                                                          href={href}
+                                                          class={{
+                                                              active:
+                                                                  pathname ===
+                                                                  href,
+                                                          }}
+                                                      >
+                                                          {item.text}
+                                                      </a>
+                                                  </li>
+                                              );
+                                          })
+                                      : null}
+                              </ul>
                           </li>
                       );
                   })
